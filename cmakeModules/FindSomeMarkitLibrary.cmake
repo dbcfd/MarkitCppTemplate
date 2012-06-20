74c1f659bd7b113d71ed552982bca49d430a1cf4
Danny Browning <bdbrowning2@gmail.com>
2012-06-20 16:21:24 -0600

    fix errors with find module and cmake for module b

--- cmakeModules/FindSomeMarkitLibrary.cmake
+++ cmakeModules/FindSomeMarkitLibrary.cmake
@@ -32,14 +32,14 @@ macro(_SML_APPEND_LIBRARIES _list _release)
    endif()
 endmacro()
 
-macro(_SML_FIND_HEADER _include_dir)
+macro(_FIND_SML_HEADER _include_dir)
 	find_path(include_dir NAMES SomeMarkitLibrary.h
 		HINTS
 			${SomeMarkitLibrary_DIR}/include
 			${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/include
 		PATH_SUFFIXES SomeMarkitLibrary
 	)
-	set(${_include_dir} ${include_dir}
+	set(${_include_dir} ${include_dir})
 endmacro()
 
 # Find the libraries
@@ -50,18 +50,29 @@ _FIND_SML_LIBRARY(SomeMarkitLibrary_LIBRARY_DEBUG  SomeMarkitLibrary_d)
 # Find the headers
 _FIND_SML_HEADER(SomeMarkitLibrary_INCLUDE_DIR)
 
-
 # handle the QUIETLY and REQUIRED arguments and set SomeMarkitLibrary_FOUND to TRUE if 
 # all listed variables are TRUE
 include(${CMAKE_CURRENT_LIST_DIR}/FindPackageHandleStandardArgs.cmake)
+set(package_required ${SomeMarkitLibrary_FIND_REQUIRED})
+set(package_quietly ${SomeMarkitLibrary_FIND_QUIETLY})
+set(SomeMarkitLibrary_FIND_REQUIRED)
+set(SomeMarkitLibrary_FIND_QUIETLY 1)
+
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(SomeMarkitLibrary DEFAULT_MSG
     SomeMarkitLibrary_LIBRARY SomeMarkitLibrary_INCLUDE_DIR)
+set(SomeMarkitLibrary_FIND_QUIETLY)
 
 if(SomeMarkitLibrary_FOUND)
    _SML_APPEND_LIBRARIES(SomeMarkitLibrary_LIBRARIES SomeMarkitLibrary_LIBRARY)
-else if(SomeMarkitLibrary_CHECKOUT_LOCATION)
+else()
+	set(SomeMarkitLibrary_FIND_REQUIRED ${package_required})
+	set(SomeMarkitLibrary_FIND_QUIETLY ${package_quietly})
+	
 	set(checkout_command "git clone https://github.com/dbcfd/SomeMarkitLibrary.git")
-	execute_process(COMMAND ${checkout_command} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/SomeMarkitLibrary)
+	execute_process(COMMAND ${checkout_command} ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/SomeMarkitLibrary 
+		RESULT_VARIABLE coRes 
+		OUTPUT_VARIABLE coOut
+		ERROR_VARIABLE coErr)
 	_FIND_SML_HEADER(SomeMarkitLibrary_INCLUDE_DIR)
 	if(SomeMarkitLibrary_INCLUDE_DIR)
 		_FIND_SML_LIBRARY(SomeMarkitLibrary_LIBRARY        SomeMarkitLibrary)
